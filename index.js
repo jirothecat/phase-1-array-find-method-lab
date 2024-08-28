e3d06e8eb7261211e463eef42ba6c599d3b441e5
Dylan Tin <dylant711@gmail.com>
2024-08-28 01:07:38 -0400

    finished array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+    let win = record.find(object => object.result === "W");
+  
+    if (win) {
+      return win.year;
+    } else {
+      return undefined;
+    }
+  }
